a50084d2bf4b84ee106dfe91d327dcc73bdce911
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-03-29 07:30:14 +0000

    eas-cli: add 18.0.1 bottle.

--- Formula/eas-cli.rb
+++ Formula/eas-cli.rb
@@ -5,6 +5,13 @@ class EasCli < Formula
   sha256 "e41ac9a13c5ba383f4567c7716f00ed2bf80d97db99341b2bf051c2e42008b9b"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/greedylabs/tap"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "18fcdc6b29b1c89d56d36f97ac4aed7549f81a61f7778e46d833ea1abf1cc487"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9b2ae3743a54231703572e0d345d04b7bd82378ff7a59fbc3f3f5a7dc7013303"
+  end
+
   depends_on "node"
 
   def install
