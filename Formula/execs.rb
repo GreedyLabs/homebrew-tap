1c5144b44a6554256971e78faa1dc7cd109d2ef5
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-03-29 08:02:40 +0000

    execs: add 0.1.9 bottle.

--- Formula/execs.rb
+++ Formula/execs.rb
@@ -5,6 +5,12 @@ class Execs < Formula
   sha256 "51103c12012721497940f082f706ff71ec020072172e56e41a8aa65a5c5a8705"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/greedylabs/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "8874bacedb2afe5c6cc89b333d5826709081812ef01131b1bcd2cb8bbfced39e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d8b1a428c752513611a2dd288ed7843a19fdcaaf23d8bd8707a401e838da2522"
+  end
+
   depends_on "go" => :build
 
   def install
