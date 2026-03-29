da7e87e5b565f70da23e85e292d9f65c84a30e17
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-03-29 15:48:16 +0900

    eas-cli: fix syntax
    Closes #1.
    
    Signed-off-by: Daeho Ro <40587651+daeho-ro@users.noreply.github.com>

--- Formula/eas-cli.rb
+++ Formula/eas-cli.rb
@@ -1,5 +1,5 @@
 class EasCli < Formula
-  desc "EAS command line tool"
+  desc "EAS command-line tool"
   homepage "https://github.com/expo/eas-cli"
   url "https://registry.npmjs.org/eas-cli/-/eas-cli-18.0.1.tgz"
   sha256 "e41ac9a13c5ba383f4567c7716f00ed2bf80d97db99341b2bf051c2e42008b9b"
