5e32a89c9dc65e23f74f45b9f889e454b1bb117f
Daeho Ro <lamanus@greedylabs.kr>
2026-04-24 14:53:14 +0900

    ncloud-cli 1.1.29_20260423

--- Formula/ncloud-cli.rb
+++ Formula/ncloud-cli.rb
@@ -1,16 +1,16 @@
 class NcloudCli < Formula
   desc "Command-line interface for Naver Cloud Platform"
   homepage "https://cli.ncloud-docs.com/docs/guide"
-  url "https://www.ncloud.com/api/support/download/files/cli/CLI_1.1.28_20260122.zip"
-  version "1.1.28"
-  sha256 "847836db4460c043be9c9b00c4bc76f2aac7a30fb8853f4646c078147b672638"
+  url "https://www.ncloud.com/api/support/download/files/cli/CLI_1.1.29_20260423.zip"
+  version "1.1.29_20260423"
+  sha256 "8d201ad26579627339ff0927a0c4741608c4605669ef4ff425d558f90865c7d2"
   license :cannot_represent
 
   livecheck do
     url "https://cli.ncloud-docs.com/docs/guide-clichange"
-    regex(/CLI[._-]v?(\d+(?:\.\d+)+)[._-](?:\d{8}).zip/i)
+    regex(/CLI[._-]v?(\d+(?:\.\d+)+)[._-](\d{8}).zip/i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match[0] }
+      page.scan(regex).map { |match| "#{match[0]}_#{match[1]}" }
     end
   end
 
@@ -23,13 +23,13 @@ class NcloudCli < Formula
   depends_on "openjdk"
 
   def install
-    jar_name = "ncloud-api-cli-#{version}-SNAPSHOT-jar-with-dependencies.jar"
+    jar_name = "ncloud-api-cli-#{version.split("_").first}-SNAPSHOT-jar-with-dependencies.jar"
     libexec.install "cli_linux/lib/#{jar_name}"
     bin.write_jar_script libexec/jar_name, "ncloud"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/ncloud --version")
+    assert_match version.split("_").first.to_s, shell_output("#{bin}/ncloud --version")
     assert_match "credentials are not set yet.", shell_output("#{bin}/ncloud vpc", 1)
   end
 end
