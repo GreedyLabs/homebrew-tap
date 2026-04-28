e5fe99a66e8286b0010c56009ce863831b4f8947
Daeho Ro <lamanus@greedylabs.kr>
2026-04-28 19:35:58 +0900

    granter-cli 0.1.3 (new formula)

---
+++ Formula/granter-cli.rb
@@ -0,0 +1,19 @@
+class GranterCli < Formula
+  desc "Granter command-line tool"
+  homepage "https://github.com/granter-biz"
+  url "https://registry.npmjs.org/@granter-biz/cli/-/cli-0.1.3.tgz"
+  sha256 "501441e55ccbe337bb0e6760c2c79a4c125709e813cdd1d6269718e587fe01f2"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"granter", "config", "--api-key", "test"
+    assert_match "유효하지 않은 API key이거나 권한이 없습니다.", shell_output("#{bin}/granter tags 2>&1", 1)
+  end
+end
